a7985d2db8bcb9007080065ade6a8b6e5e408784
Leonardo Milão <leonardoo.milao17@gmail.com>
2024-03-12 12:00:01 -0300

    Terceira tela de checkout

---
+++ view/nota_fiscal.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nota Fiscal</title>
+    <link rel="stylesheet" href="../public/css/check.css">
+</head>
+<body>
+    <div class="container-2" >
+        <div class="section" style="height: 500px">
+            <h2>Nota Fiscal</h2>
+            <div id="invoiceDetails ">
+                <div class="linha-2">
+                    <p><strong>Nome do Produto:</strong> <?php echo $_POST['productName']; ?></p>
+                    <p><strong>Quantidade:</strong> <?php echo $_POST['quantity']; ?></p>
+                
+                </div
+                <div class="linha-2">
+                    <p><strong>Modelo:</strong> <?php echo $_POST['model']; ?></p>
+                    <p><strong>Cor:</strong> <?php echo $_POST['color']; ?></p>
+                </div>
+                <p><strong>Forma de Pagamento:</strong> <?php echo $_POST['paymentMethod']; ?></p>
+                <?php if ($_POST['paymentMethod'] === 'credito' || $_POST['paymentMethod'] === 'debito'): ?>
+                    <p><strong>Número do Cartão:</strong> <?php echo $_POST['cardNumber']; ?></p>
+                <?php endif; ?>
+                <div class="linha-3">
+                    <p><strong>Nome:</strong> <?php echo $_POST['name']; ?></p>
+                    <p><strong>CPF:</strong> <?php echo $_POST['cpf']; ?></p>
+                    
+                    
+                </div>
+                <div class="linha-3">
+                <p><strong>E-mail:</strong> <?php echo $_POST['email']; ?></p>
+                <p><strong>CEP:</strong> <?php echo $_POST['cep']; ?></p>
+                <p><strong>Número:</strong> <?php echo $_POST['number']; ?></p>
+                    
+                </div
+                <p><strong>Valor:</strong> <?php echo $_POST['valor']; ?></p>
+                
+                </div>
+            <button onclick="window.print()">Imprimir Nota Fiscal</button>
+        </div>
+    </div>
+</body>
+</html>
